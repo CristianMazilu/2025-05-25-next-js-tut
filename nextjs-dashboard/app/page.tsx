563a72de1a3e960f1bbf8ad8756fbd72a12b3f4f
Cristian Mazilu <mazilu6@gmail.com>
2025-05-26 23:11:22 +0300

    Temporary change

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -33,7 +33,7 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <Image
               src="/hero-desktop.png"
-              width={1000}
+              width={1001}
               height={760}
               alt="Screenshots of dashboard project."></Image>
           <AcmeLogo/>
